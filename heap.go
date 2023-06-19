5cff3e90ae85c1f2a6e129584257c7131c4f2449
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-06-19 15:04:28 +0330

    refactor: update methods and add godocs

--- heap.go
+++ heap.go
@@ -1,32 +1,38 @@
 package heap
 
+// node is an instantiation of heap's elements that contains value and alias.
 type node struct {
 	value int
 	alias any
 }
 
+// tree is an instantiation of a binary heap tree.
 type tree struct {
 	nodes []*node
 }
 
+// MinHeap is an instantiation of a min binary heap .
 type MinHeap struct {
 	tree *tree
 }
 
+// MaxHeap is an instantiation of a max binary heap .
 type MaxHeap struct {
 	tree *tree
 }
 
-func parent(index int) int {
-	if index == 0 {
+// parent returns index of the parent node of inputted node.
+func parent(nodeIndex int) int {
+	if nodeIndex == 0 {
 		return -1
 	}
 
-	return (index - 1) / 2
+	return (nodeIndex - 1) / 2
 }
 
-func leftChild(tree *tree, index int) (*node, int) {
-	l := (2 * index) + 1
+// leftChild returns left child node and index of the inputted node.
+func leftChild(tree *tree, nodeIndex int) (*node, int) {
+	l := (2 * nodeIndex) + 1
 	if l >= len(tree.nodes) {
 		return nil, -1
 	}
@@ -34,8 +40,9 @@ func leftChild(tree *tree, index int) (*node, int) {
 	return tree.nodes[l], l
 }
 
-func rightChild(tree *tree, index int) (*node, int) {
-	r := (2 * index) + 2
+// rightChild returns left child node and index of the inputted node.
+func rightChild(tree *tree, nodeIndex int) (*node, int) {
+	r := (2 * nodeIndex) + 2
 	if r >= len(tree.nodes) {
 		return nil, -1
 	}
@@ -43,11 +50,13 @@ func rightChild(tree *tree, index int) (*node, int) {
 	return tree.nodes[r], r
 }
 
+// swap changes position of two node in the inputted tree.
 func swap(tree *tree, index1 int, index2 int) {
 	tree.nodes[index1], tree.nodes[index2] = tree.nodes[index2], tree.nodes[index1]
 	return
 }
 
+// Insert add new node to the tree.
 func (h *MinHeap) Insert(value int, alias ...any) {
 	var a any
 	if len(alias) > 0 {
@@ -77,6 +86,7 @@ func (h *MinHeap) Insert(value int, alias ...any) {
 	}
 }
 
+// Insert add new node to the tree.
 func (h *MaxHeap) Insert(value int, alias ...any) {
 	var a any
 	if len(alias) > 0 {
@@ -105,6 +115,7 @@ func (h *MaxHeap) Insert(value int, alias ...any) {
 	}
 }
 
+// Delete removes root and updates (heapify) tree.
 func (h *MinHeap) Delete() {
 	if len(h.tree.nodes) == 0 {
 		return
@@ -122,6 +133,7 @@ func (h *MinHeap) Delete() {
 	h.heapify(0)
 }
 
+// Delete removes root and updates (heapify) tree.
 func (h *MaxHeap) Delete() {
 	if len(h.tree.nodes) == 0 {
 		return
@@ -139,48 +151,50 @@ func (h *MaxHeap) Delete() {
 	h.heapify(0)
 }
 
-func (h *MinHeap) heapify(index int) {
+// heapify rearranged the tree's nodes to maintain the heap.
+func (h *MinHeap) heapify(rootIndex int) {
 	for {
-		current := h.tree.nodes[index]
+		current := h.tree.nodes[rootIndex]
 
-		left, leftIndex := leftChild(h.tree, index)
-		right, rightIndex := rightChild(h.tree, index)
+		left, leftIndex := leftChild(h.tree, rootIndex)
+		right, rightIndex := rightChild(h.tree, rootIndex)
 		if left == nil && right == nil {
 			break
 		} else if left == nil && right != nil {
 			if current.value < right.value {
 				break
 			} else {
-				swap(h.tree, index, rightIndex)
-				index = rightIndex
+				swap(h.tree, rootIndex, rightIndex)
+				rootIndex = rightIndex
 			}
 		} else if right == nil && left != nil {
 			if current.value < left.value {
 				break
 			} else {
-				swap(h.tree, index, leftIndex)
-				index = leftIndex
+				swap(h.tree, rootIndex, leftIndex)
+				rootIndex = leftIndex
 			}
 		} else {
 			if left.value >= current.value && right.value >= current.value {
 				break
 			} else if left.value < right.value && left.value < current.value {
-				swap(h.tree, index, leftIndex)
-				index = leftIndex
+				swap(h.tree, rootIndex, leftIndex)
+				rootIndex = leftIndex
 			} else {
-				swap(h.tree, index, rightIndex)
-				index = rightIndex
+				swap(h.tree, rootIndex, rightIndex)
+				rootIndex = rightIndex
 			}
 		}
 	}
 }
 
-func (h *MaxHeap) heapify(index int) {
+// heapify rearranged the tree's nodes to maintain the heap.
+func (h *MaxHeap) heapify(rootIndex int) {
 	for {
-		current := h.tree.nodes[index]
+		current := h.tree.nodes[rootIndex]
 
-		left, leftIndex := leftChild(h.tree, index)
-		right, rightIndex := rightChild(h.tree, index)
+		left, leftIndex := leftChild(h.tree, rootIndex)
+		right, rightIndex := rightChild(h.tree, rootIndex)
 
 		if left == nil && right == nil {
 			break
@@ -188,45 +202,49 @@ func (h *MaxHeap) heapify(index int) {
 			if current.value > right.value {
 				break
 			} else {
-				swap(h.tree, index, rightIndex)
-				index = rightIndex
+				swap(h.tree, rootIndex, rightIndex)
+				rootIndex = rightIndex
 			}
 		} else if right == nil && left != nil {
 			if current.value > left.value {
 				break
 			} else {
-				swap(h.tree, index, leftIndex)
-				index = leftIndex
+				swap(h.tree, rootIndex, leftIndex)
+				rootIndex = leftIndex
 			}
 		} else {
 			if left.value <= current.value && right.value <= current.value {
 				break
 			} else if left.value > right.value && left.value > current.value {
-				swap(h.tree, index, leftIndex)
-				index = leftIndex
+				swap(h.tree, rootIndex, leftIndex)
+				rootIndex = leftIndex
 			} else {
-				swap(h.tree, index, rightIndex)
-				index = rightIndex
+				swap(h.tree, rootIndex, rightIndex)
+				rootIndex = rightIndex
 			}
 		}
 	}
 }
-func (h *MinHeap) GetMin() int {
+
+// GetMin returns min value (root) from heap tree with alias.
+func (h *MinHeap) GetMin() (int, any) {
 	if len(h.tree.nodes) == 0 {
-		return 0
+		return 0, nil
 	}
 
-	return h.tree.nodes[0].value
+	return h.tree.nodes[0].value, h.tree.nodes[0].alias
 }
 
-func (h *MaxHeap) GetMax() int {
+// GetMax returns max value (root) from heap tree with alias.
+func (h *MaxHeap) GetMax() (int, any) {
 	if len(h.tree.nodes) == 0 {
-		return 0
+		return 0, nil
 	}
 
-	return h.tree.nodes[0].value
+	return h.tree.nodes[0].value, h.tree.nodes[0].alias
 }
 
+// NewMin creates a new instance of a min binary heap.
 func NewMin() *MinHeap {
 	return &MinHeap{
 		tree: &tree{
@@ -235,6 +253,7 @@ func NewMin() *MinHeap {
 	}
 }
 
+// NewMax creates a new instance of a max binary heap.
 func NewMax() *MaxHeap {
 	return &MaxHeap{
 		tree: &tree{
